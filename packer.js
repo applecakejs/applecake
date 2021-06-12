dedf9d3493df9d90e22c7f6e40c3165d93eaf582
mehan <mehan.alavi.88.majd@gmail.com>
2021-06-12 12:54:15 +0430

    Added chalk to packer

--- packer.js
+++ packer.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const chalk = require("chalk")
 let data = "";
 
 const Pack = (arrayOfJsfiles , dist , version) => {
@@ -6,10 +7,11 @@ const Pack = (arrayOfJsfiles , dist , version) => {
   for (const file of arrayOfJsfiles) {
     let fileread = fs.readFileSync(file);
     data += fileread + "\n";
-    console.log(`writing file ${file} is done`);
+    
+    console.log(chalk.cyanBright(`writing file ${file} is done`));
   }
   console.log("\n");
-  console.log("everything is done");
+  console.log(chalk.greenBright("everything is done"));
   fs.writeFileSync(`${dist} ${version}.js`, data);
 };
 
