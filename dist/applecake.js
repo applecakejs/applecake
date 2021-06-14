46ddd4f830500bd27257e1878a914a19a056091b
mehan <mehan.alavi.88.majd@gmail.com>
2021-06-14 13:17:28 +0430

    added delete to dist

--- dist/applecake.js
+++ dist/applecake.js
@@ -17,8 +17,14 @@ function Insert(id , html) {
 }
 const IdManager = {
   edit: function (oldid , newid) {
+    // edits a html element id
     let select = document.getElementById(`${oldid}`);
     select.id = newid
+  } , 
+  delete : function (oldid){
+    // deletes a html elements id
+    let select = document.getElementById(`${oldid}`);
+    select.id = ""
   }
 };
 
