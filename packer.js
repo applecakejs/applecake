8563064fa9b1a2317c4146e749d9e454c74839a4
mehan <mehan.alavi.88.majd@gmail.com>
2021-06-14 12:29:58 +0430

    update packer

--- packer.js
+++ packer.js
@@ -1,20 +1,23 @@
 const fs = require("fs");
-const chalk = require("chalk")
+const chalk = require("chalk");
 let data = "";
 
-const Pack = (arrayOfJsfiles , dist , version) => {
-
+const Pack = (arrayOfJsfiles, filenamedist) => {
   for (const file of arrayOfJsfiles) {
     let fileread = fs.readFileSync(file);
     data += fileread + "\n";
-    
+
     console.log(chalk.cyanBright(`writing file ${file} is done`));
   }
   console.log("\n");
   console.log(chalk.greenBright("everything is done"));
-  fs.writeFileSync(`${dist} ${version}.js`, data);
+  if (fs.existsSync("./dist")) {
+    fs.rmdirSync("./dist" , {"recursive" : true})
+  }
+  fs.mkdirSync("dist");
+  fs.writeFileSync(`dist/${filenamedist}.js`, data);
 };
 
 module.exports = {
-    Pack
-}
+  Pack,
+};
